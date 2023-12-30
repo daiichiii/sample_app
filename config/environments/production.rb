8aff1fcfe0ecdbc65d2aadfe7febf723bd62dc29
daiichiii <136941425+daiichiii@users.noreply.github.com>
2023-12-30 16:03:09 +0000

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,17 +1,18 @@
 class Micropost < ApplicationRecord
   belongs_to :user
 end
+
 Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<https://dashboard.render.com/web/srv-ckcl1fmsmu8c73d9ktug/deploys/dep-cm83bp21hbls73av8ll0>.onrender.com'
+  host = 'https://rails-sample-app-gw0e.onrender.com'  # レンダーのホスト名に変更してください
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :port           => 587,
-    :address        => 'smtp.mailgun.org',
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => host,
-    :authentication => :plain,
+    port:           587,
+    address:        'smtp.mailgun.org',
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         'your-domain.com',  # メール送信元のドメインに変更してください
+    authentication: :plain,
   }
 end
