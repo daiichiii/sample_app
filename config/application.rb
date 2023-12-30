ab171da38db83c8a8cfcb4edad222eb83e6028e6
daiichiii <136941425+daiichiii@users.noreply.github.com>
2023-12-30 16:17:33 +0000

    configを修正

--- config/application.rb
+++ config/application.rb
@@ -6,5 +6,8 @@ module SampleApp
   class Application < Rails::Application
     config.load_defaults 7.0
     config.active_storage.variant_processor = :mini_magick
+
+    # config.autoload_paths += %W(#{config.root}/app/models)
+    config.eager_load_paths += %W(#{config.root}/app/models)
   end
 end
