c45cd3d2b9bc283f7e26110ff94a64ecfbf33c6a
daiichiii <136941425+daiichiii@users.noreply.github.com>
2024-03-27 02:24:51 +0000

    Revert "検索機能の拡張"
    This reverts commit 76e4c96b088ff911c139e4dbd91d7575159c8cb0.

--- app/models/user.rb
+++ app/models/user.rb
@@ -109,10 +109,6 @@ has_many :passive_relationships, class_name:  "Relationship",
     following.include?(other_user)
   end
 
-  def self.ransackable_attributes(auth_object = nil)
-    ["activated", "activated_at", "activation_digest", "admin", "created_at", "email", "id", "name", "password_digest", "remember_digest", "reset_digest", "reset_sent_at", "updated_at"]
-  end
-
   private
 
   # メールアドレスをすべて小文字にする
